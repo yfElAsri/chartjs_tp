ddd106f553887248fb6e8d8c5f90666ec9e77c77
yfElAsri <yf.elasri@gmail.com>
2021-06-09 12:10:21 +0200

    2 files modified

--- index.js
+++ index.js
@@ -2,6 +2,120 @@
 import './style.css';
 import Chart from 'chart.js';
 
+let nbDemande = document.getElementById('nbDemande').getContext('2d');
+let linechartDemande = new Chart(nbDemande, {
+  type: 'line',
+  data: {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'Nb de demande en 2020',
+        data: [650, 590, 800, 810, 560, 550, 400],
+        fill: true,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
+      }
+    ]
+  }
+});
+
+let freqPort = document.getElementById('freqPort').getContext('2d');
+let barchartPort = new Chart(freqPort, {
+  type: 'bar',
+  data: {
+    labels: [
+      'HAVRE(LE)',
+      'ANVERS',
+      'MARSEILLE',
+      'FOS-SUR-MER',
+      'MONTOIR-DE-BRETAGNE',
+      'ROTTERDAM',
+      'DUNKERQUE',
+      'ZEEBRUGGE',
+      'BASSENS'
+    ],
+    datasets: [
+      {
+        label: 'nb de demande avec x Port',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 205, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(201, 203, 207, 0.2)'
+        ],
+        borderColor: [
+          'rgb(255, 99, 132)',
+          'rgb(255, 159, 64)',
+          'rgb(255, 205, 86)',
+          'rgb(75, 192, 192)',
+          'rgb(54, 162, 235)',
+          'rgb(153, 102, 255)',
+          'rgb(201, 203, 207)'
+        ],
+        borderWidth: 1
+      }
+    ]
+  }
+});
+
+let ImpvsExpo = document.getElementById('ImpvsExpo').getContext('2d');
+let horibarchartImp = new Chart(ImpvsExpo, {
+  type: 'bar',
+  data: {
+    labels: [
+      'Import',
+      'Export'
+    ],
+    datasets: [
+      {
+        axis: 'y',
+        label: 'nb de demande',
+        data: [1239, 1658],
+        fill: false,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)'
+        ],
+        borderColor: [
+          'rgb(255, 99, 132)',
+          'rgb(54, 162, 235)'
+        ],
+        borderWidth: 1
+      }
+    ]
+  },
+  options: {
+    indexAxis: 'y',
+  }
+});
+
+let chart12 = document.getElementById('chart12').getContext('2d');
+let piechart12 = new Chart(chart12, {
+  type: 'doughnut',
+  data: {
+    labels: [
+    '40',
+    '20',
+    '40HC'
+  ],
+    datasets: [{
+    label: 'Type TC',
+    data: [300, 50, 100],
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(54, 162, 235)',
+      'rgb(255, 205, 86)'
+    ],
+    hoverOffset: 4
+  }]
+  }
+});
+
+
 import * as DataSet1 from './data/AU-city-population-2018.json';
 
 
@@ -70,85 +184,6 @@ let barChart2 = new Chart(chart2, {
   options: {}
 });
 
-let chart10 = document.getElementById('chart10').getContext('2d');
-let linechart10 = new Chart(chart10, {
-  type: 'line',
-  data: {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [
-      {
-        label: 'Nb de demande en 2020',
-        data: [65, 59, 80, 81, 56, 55, 40],
-        fill: true,
-        borderColor: 'rgb(75, 192, 192)',
-        tension: 0.1
-      }
-    ]
-  }
-});
 
-let chart11 = document.getElementById('chart11').getContext('2d');
-let barchart11 = new Chart(chart11, {
-  type: 'bar',
-  data: {
-    labels: [
-      'HAVRE(LE)',
-      'ANVERS',
-      'MARSEILLE',
-      'FOS-SUR-MER',
-      'MONTOIR-DE-BRETAGNE',
-      'ROTTERDAM',
-      'DUNKERQUE',
-      'ZEEBRUGGE',
-      'BASSENS'
-    ],
-    datasets: [
-      {
-        label: 'Port par feq',
-        data: [65, 59, 80, 81, 56, 55, 40],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-          'rgba(255, 205, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(201, 203, 207, 0.2)'
-        ],
-        borderColor: [
-          'rgb(255, 99, 132)',
-          'rgb(255, 159, 64)',
-          'rgb(255, 205, 86)',
-          'rgb(75, 192, 192)',
-          'rgb(54, 162, 235)',
-          'rgb(153, 102, 255)',
-          'rgb(201, 203, 207)'
-        ],
-        borderWidth: 1
-      }
-    ]
-  }
-});
 
-let chart12 = document.getElementById('chart12').getContext('2d');
-let piechart12 = new Chart(chart12, {
-  type: 'doughnut',
-  data: {
-    labels: [
-    '40',
-    '20',
-    '40HC'
-  ],
-    datasets: [{
-    label: 'Type TC',
-    data: [300, 50, 100],
-    backgroundColor: [
-      'rgb(255, 99, 132)',
-      'rgb(54, 162, 235)',
-      'rgb(255, 205, 86)'
-    ],
-    hoverOffset: 4
-  }]
-  }
-});
 
